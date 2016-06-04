2d41931b5a71722915fbace0fc413f9697e205f7
dimiii <dmalykh@webzilla.com>
2016-06-04 18:37:45 +0300

    Check time arguments.

--- web.rb
+++ web.rb
@@ -32,11 +32,13 @@ class RestResource < Sinatra::Base
 
   get '/pingstat/summary/:ip' do
     host = params['ip']
-    from = params['from'].to_i
-    to   = params['to'].to_i   #TODO from/to checking
+    beginPeriod = params['from'].to_i
+    endPeriod   = params['to'].to_i
     halt 400, "Expected ipv4 address, got:#{host}" unless IPAddress::valid_ipv4? host
+    halt 400, "Expected number of epoch seconds, got:#{beginPeriod}" unless /^\d+$/ =~ beginPeriod or beginPeriod.nil?
+    halt 400, "Expected number of epoch seconds, got:#{endPeriod}" unless /^\d+$/ =~ endPeriod or endPeriod.nil?
 
-    @service.summary(host, from, to)
+    @service.summary(host, beginPeriod, endPeriod)
   end
 
   get '/pingstat/op-hosts' do
