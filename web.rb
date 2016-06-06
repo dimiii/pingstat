df1a7927c46288cf339b3a7243939d79ff153da4
dimiii <dmalykh@webzilla.com>
2016-06-06 09:39:08 +0300

    [FIX] Initialization of Sinatra.

--- web.rb
+++ web.rb
@@ -11,10 +11,7 @@ class RestResource < Sinatra::Base
   set :logging, true
   set :port, 8080
 
-  def initialize(app = nil, service: PingDaemon.new(hostStorage: InRedis.new(Redis.new), operated: true))
-    super(app)
-    @service = service
-  end
+  service = PingDaemon.new(hostStorage: InRedis.new(Redis.new, batchSize: 100), pingFrequency: 60, operated: true)
 
   get '/pingstat' do
     'Hi!'
@@ -24,14 +21,14 @@ class RestResource < Sinatra::Base
     host = params['ip']
     halt 400, "Expected ipv4 address, got:#{host}" unless IPAddress::valid_ipv4? host
 
-    @service.add(host)
+    service.add(host)
   end
 
   put '/pingstat/del/:ip' do
     host = params['ip']
     halt 400, "Expected ipv4 address, got:#{host}"  unless IPAddress::valid_ipv4? host
 
-    @service.delete(host)
+    service.delete(host)
   end
 
   get '/pingstat/summary/:ip' do
@@ -44,16 +41,15 @@ class RestResource < Sinatra::Base
     halt 400, "Expected number of epoch seconds, got:#{endPeriod}" unless endPeriod.nil? or /^\d+$/ =~ params['to']
     halt 400, 'Expected precedence of time' unless beginPeriod.nil? or endPeriod.nil? or beginPeriod < endPeriod
 
-    summary = @service.summary(host, beginPeriod && beginPeriod.to_i, endPeriod && endPeriod.to_i)
+    summary = service.summary(host, beginPeriod && beginPeriod.to_i, endPeriod && endPeriod.to_i)
     halt 404, 'No data for period' if summary.err?
 
     summary.to_hash.to_json
   end
 
   get '/pingstat/op-hosts' do
-    @service.tasks.keys.to_json
+    service.tasks.keys.to_json
   end
 
   run! if __FILE__ == $0
 end
-
